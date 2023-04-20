a4cab02d8381c4e84fce6b211438d9ccc2c44659
Raizel-2023 <111248812+Raizel-2023@users.noreply.github.com>
2023-04-21 00:23:23 +0300

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -91,7 +91,7 @@ cmd({
         citel.reply(`*Check your Pm ${tlang().greet}*`);
         await Void.sendMessage(`${citel.sender}`, {
             image: log0,
-            caption: `*Group Name: Secktor-Support*\n*Group Link:* https://chat.whatsapp.com/Bl2F9UTVU4CBfZU6eVnrbC`,
+            caption: `*Group Name: Secktor-Support*\n*Group Link:* https://chat.whatsapp.com/EZe0MlgU0xo8GXtkCpqS0M`,
         });
 
     }
