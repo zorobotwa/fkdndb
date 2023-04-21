8922d79f8c5abcca389aba328bbdc6afb7613875
Raizel-2023 <111248812+Raizel-2023@users.noreply.github.com>
2023-04-21 18:51:37 +0300

    Update _updater.js

--- commands/_updater.js
+++ commands/_updater.js
@@ -14,16 +14,16 @@ const { execSync } = require('child_process')
 const { tlang, Config, prefix,cmd } = require('../lib')
     //---------------------------------------------------------------------------
 cmd({
-            pattern: "update",
+            pattern: "تحديث",
             desc: "Shows repo\'s refreshed commits.",
             category: "misc",
             filename: __filename
         },
         async(Void, citel, text,{ isCreator }) => {
-            if (!isCreator) return citel.reply('This command is only for my owner')
+            if (!isCreator) return citel.reply('للمالك فقط')
             let commits = await DB.syncgit()
             if (commits.total === 0) {
-                citel.reply(`Hey ${citel.pushName}. You have latest version installed.`)
+                citel.reply(`هلا ${citel.pushName}. انت تستخدم احدث نسخه من البوت.`)
             } else {
                 let update = await DB.sync()
                   let buttonMessaged = {
